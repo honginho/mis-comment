966e9a7e19887b31c36d1990336aeeb9e5d5df8a
honginho <sm870831@gmail.com>
2019-06-03 11:33:02 +0800

    Fine-tuning.

--- query.php
+++ query.php
@@ -298,10 +298,8 @@ if (isset($_GET['comments_id'])) {
                 }
 
                 let otherComments = '<?php echo $comments_details["other_comment"]; ?>'.split('@,|,@');
-                console.log(otherComments);
                 for (let i = 0; i < otherComments.length; i++) {
                     otherCommentsDetails = otherComments[i].split('@-|-@');
-                    console.log(otherCommentsDetails)
                     $('.other-comment[data-index="' + otherCommentsDetails[0] + '"]').text(otherCommentsDetails[1]);
                 }
             </script>
