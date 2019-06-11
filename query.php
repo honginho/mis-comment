966be145b52fce2f9de7b140e01a93dfd492f872
honginho <sm870831@gmail.com>
2019-06-11 17:16:01 +0800

    Debuging...... 10806114

--- query.php
+++ query.php
@@ -245,7 +245,7 @@ if (isset($_GET['comments_id'])) {
                 <nav>
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
 <?php
-        var_dump($arr_comments_all); die();
+        // var_dump($arr_comments_all); die();
         for ($i = 0; $i < count($arr_comments_all); $i++) {
             $m = $arr_comments_all[$i][0][0]; // `main`
             $s = $arr_comments_all[$i][0][1]; // `sub`
